cbf93a664c71fad1f9de0d5dd1a2ecfeb6ac1671
swnb <1786718956@qq.com>
2018-10-25 14:39:44 +0800

    update elisp deps packages

--- elpa/swiper-20181008.1731/swiper-pkg.el
+++ elpa/swiper-20181008.1731/swiper-pkg.el
@@ -1,2 +1,2 @@
 ;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20181008.1731" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "59b896699e54cc8b40998dc284da5694f128fb60" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
+(define-package "swiper" "20181008.1731" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "d3469919cdfdd6165a83b818df08feea50181012" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
